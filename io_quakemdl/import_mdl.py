13fda66334b689c9f9c5174887174e02459c4e5f
Eric Chan <ettchan618@gmail.com>
2013-11-26 22:48:32 -0800

    debugging problem with importer

---
+++ io_quakemdl/import_mdl.py
@@ -0,0 +1,20 @@
+import re
+import bpy
+import mdl
+
+def import_mdl(operator, context, filepath):
+
+	# keeps a copy of the file in memory
+	#bpy.context.user_preferences.edit.use_global_undo = False
+
+	# deactivate objects in the scene
+	#for obj in bpy.types.scene.objects:
+		#obj = False
+
+	#mdl = MDL();
+	operator.report({'ERROR'}, "Inside import_mdl")
+	return {'CANCEL'}
+	#if not read_mdl(filename):
+		
+
+	#return {'FINISHED'}
